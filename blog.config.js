06056c996174de7b0d86f53e442a51246a554c9a
WilliamK7 <82304956+WilliamK7@users.noreply.github.com>
2023-09-17 17:57:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -45,7 +45,7 @@ const BLOG = {
   telegramChatId: '5558125873', // The chat id of your Telegram bot
   telegramChannelUrl: 'https://t.me/MoonBase_WK', // The link of your Telegram channel
   telegramChannelName: 'Moon Base', // The name of your Telegram channel
-  craftConfigShareUrl: '', // The link to share your craft config
+  craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
     ackeeConfig: {
